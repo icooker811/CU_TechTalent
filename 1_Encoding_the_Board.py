06e6c97748c57187499ae1155391434152c2a6c0
icooker811 <icooker811@gmail.com>
2017-09-04 20:21:49 +0700

    initial

---
+++ 1_Encoding_the_Board.py
@@ -0,0 +1,97 @@
+'''
+Exercise1: Encoding the Board
+>>> from utils import display
+>>> display(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))
+. . 3 |. 2 . |6 . . 
+9 . . |3 . 5 |. . 1 
+. . 1 |8 . 6 |4 . . 
+------+------+------
+. . 8 |1 . 2 |9 . . 
+7 . . |. . . |. . 8 
+. . 6 |7 . 8 |2 . . 
+------+------+------
+. . 2 |6 . 9 |5 . . 
+8 . . |2 . 3 |. . 9 
+. . 5 |. 1 . |3 . .
+'''
+
+#1. utils.py ----------------------------
+#1.1 define rows: 
+rows = 'ABCDEFGHI'
+
+#1.2 define cols:
+cols = '123456789'
+
+#1.3 cross(a,b) helper function to create boxes, row_units, column_units, square_units, unitlist
+def cross(a, b):
+    return [s+t for s in a for t in b]
+
+#1.4 create boxes
+boxes = cross(rows, cols)
+
+#1.5 create row_units
+row_units = [cross(r, cols) for r in rows]
+
+#1.6 create column_units
+column_units = [cross(rows, c) for c in cols]
+
+#1.7 create square_units for 9x9 squares
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+#1.8 create unitlist for all units
+unitlist = row_units + column_units + square_units
+
+#1.9 create peers of a unit from all units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+#1.10 display function receiving "values" as a dictionary and display a 9x9 suduku board
+def display(values):
+    """
+    Display the values as a 2-D grid.
+    Input: The sudoku in dictionary form
+    Output: None
+    """
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
+    
+
+#2. function.py ----------------------------
+'''
+Instruction: create grid_values(grid) A function to convert the string representation 
+of a puzzle into a dictionary form.
+'''
+#2.1 grid_values function (input> strong of sudoku problem, output> dictionary of a suduku with corresponding boxes) 
+#from utils import *  #remove this when use as a file
+def grid_values(grid):
+    # In this function, you will take a sudoku as a string
+    # and return a dictionary where the keys are the boxes,
+    # for example 'A1', and the values are the digit at each
+    # box (as a string) or '.' if the box has no value
+    # assigned yet.
+    
+    ''' Your solution here 
+    .........
+    .........
+    .........
+    .........
+    .........
+    .........
+    '''
+    values = {}
+    i = 0
+    for s in boxes:
+        values[s] = grid[i]
+        i += 1
+    return values
+    
+#3. Test function.py ----------------------------    
+# print(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))
+
+#4. Test utils.py ---------------------------- 
+display(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))
